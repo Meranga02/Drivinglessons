d5a2e5cb0467898f5d6b8f5a71c6c3f1ac7e0abf
Meranga02 <tushanherath.napolisolutions@gmail.com>
2022-10-02 22:32:17 +0530

    Validation added

--- app/src/main/java/com/example/drivinglesson/LoginPage.java
+++ app/src/main/java/com/example/drivinglesson/LoginPage.java
@@ -2,6 +2,8 @@ package com.example.drivinglesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void resetpassword(View view) {
+        Toast.makeText(LoginPage.this,"Reset your password", Toast.LENGTH_LONG).show();
+    }
+
+    public void register(View view) {
+        Toast.makeText(LoginPage.this,"Please Register First", Toast.LENGTH_LONG).show();
     }
 }
